a6116b66afbf1d1b5d058e076937a9065ee48c3d
Moditha <moditha88@gmail.com>
2016-12-27 20:01:43 +0100

    Retrieving users and tweets with id

---
+++ src/main/java/ecp/reputation/tweets/TwitterDownloader.java
@@ -0,0 +1,35 @@
+package ecp.reputation.tweets;
+
+import twitter4j.ResponseList;
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.User;
+
+public class TwitterDownloader {
+
+	Twitter twitter = TwitterFactory.getSingleton();
+
+	public Status getTweetWithId(long id) {
+		Status s = null;
+		try {
+			s = twitter.showStatus(id);
+		} catch (TwitterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return s;
+	}
+
+	public ResponseList<User> getUserWithId(String screenNames) {
+		ResponseList<User> list = null;
+		try {
+			list = twitter.lookupUsers(screenNames);
+		} catch (TwitterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return list;
+	}
+}

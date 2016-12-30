fd093ce0ce0ef39a8ff65ab1400ca329b857e591
Suela <isuela@ulb.ac.be>
2016-12-30 18:26:59 +0100

    followers

--- src/test/java/TestWrite.java
+++ src/test/java/TestWrite.java
@@ -20,7 +20,7 @@ public class TestWrite {
 		List<Status> statuses;
 		try {
 //			Paging page = new Paging(1, 500);
-			Query query = new Query("trump");
+			Query query = new Query("tesla model x");
 			// if(result.hasNext())//there is more pages to load
 			// {
 			// query = result.nextQuery();
